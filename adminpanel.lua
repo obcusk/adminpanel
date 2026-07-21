8bdd4f25edd5f4a1289ada6d62045575123c0f59
obcusk <franciscaigomezt23@gmail.com>
2026-07-21 00:38:28 -0400

    Update adminpanel.lua

--- adminpanel.lua
+++ adminpanel.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://oblivionhub.xyz/api/paste?id=775b713937082f7fa6639477653910bd&raw=true"))()
+loadstring(game:HttpGet("https://oblivionhub.xyz/api/paste?id=5542269965d9c700f02943e870522da5&raw=true"))()
