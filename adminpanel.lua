d7f9765d3e4fa8fd8e7101fdd7e2445f35cfadb8
obcusk <franciscaigomezt23@gmail.com>
2026-07-24 01:57:53 -0400

    Update adminpanel.lua

--- adminpanel.lua
+++ adminpanel.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/5e46f365528438e8519416c06ea9961c.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/24c71ebd0b3bf768bc30ad2b3703778e.lua"))()
